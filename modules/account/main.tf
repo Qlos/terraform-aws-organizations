561d7b3ad783f31c844dfbe4f7f59472b73bdfce
Marcin Serek <marcin.serek@qlos.com>
2024-05-28 14:09:35 +0200

    chore: add lifecycle policy to avoid problems when running terraform second time

--- modules/account/main.tf
+++ modules/account/main.tf
@@ -4,4 +4,9 @@ resource "aws_organizations_account" "this" {
   close_on_deletion = var.close_on_deletion
   role_name = var.role_name
   tags = var.tags
+
+  lifecycle {
+    ignore_changes = [ role_name ]
+  }
 }
+
